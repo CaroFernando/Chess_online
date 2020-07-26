a20720ed4a4922a6034272516d936b1a19503ee4
Lalito Robles <laloro08@hotmail.com>
2020-07-25 20:01:09 -0500

    Some pieces' movement implemented
    The behavior of rook, knight, king and pawn has been implemented

--- game.py
+++ game.py
@@ -9,14 +9,24 @@ def PossibleMovements(piece):
     if(piece[0][0] == "r"):                                 #Movimientos para el rey
         for i in range(piece[1][0]-1, piece[1][0]+2):       
             for j in range(piece[1][1]-1, piece[1][1]+2):
-                if (i, j) != piece[1]: 
-                    movements.append((i, j))
+                movements.append((i, j))
     #elif(piece[0][0] == "d"):
-    #elif(piece[0][0] == "t"):
-    #elif(piece[0][0] == "a"):
-    #elif(piece[0][0] == "c"):
-    #elif(piece[0][0] == "p"):        
+    elif(piece[0][0] == "t"):
+        for i in range(8):
+            movements.append((piece[1][0], i))
+            movements.append((i, piece[1][1]))
+    #elif(piece[0][0] == "a"):        
+    elif(piece[0][0] == "c"):
+        for i in range(4):
+            movements.append((piece[1][0] + (2 if i % 2 else -2), piece[1][1] + (1 if i < 2 else -1)))
+            movements.append((piece[1][0] + (1 if i % 2 else -1), piece[1][1] + (2 if i < 2 else -2)))
+    elif(piece[0][0] == "p"):   
+        movements.append((piece[1][0], piece[1][1] + (1 if piece[0][1] == "n" else -1)))
+        if (piece[1][1] == 1 and piece[0][1] == "n") or (piece[1][1] == 6):
+            movements.append((piece[1][0], piece[1][1] + (2 if piece[0][1] == "n" else -2)))
+    while piece[1] in movements: movements.remove(piece[1])
     print(movements)
+    return movements
         
 pygame.init()
 win = pygame.display.set_mode((1000, 800))
@@ -69,7 +79,6 @@ run = True
 
 while run:
     pygame.time.delay(100)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -86,7 +95,7 @@ while run:
                         if(state[i][j][0] == tabx and state[i][j][1] == taby): 
                             print(i) 
                             pieza_mov = (i, j)
-                            PossibleMovements((i, state[i][j]))
+                            mov = PossibleMovements((i, state[i][j]))
                             dragdrop = False
             else:
                 state[pieza_mov[0]][pieza_mov[1]] = (tabx, taby)
@@ -94,17 +103,17 @@ while run:
 
                 sendmsg = json.dumps(state)
                 print(sendmsg)
-            
 
     for i in range(0, 8):
         for j in range(0, 8):
             if (i+j) % 2 != 0: pygame.draw.rect(win, (70, 130, 180), (tam*i + tab_x_off, tam*j + tab_y_off, tam, tam));
             else: pygame.draw.rect(win, (255, 255, 255), (tam*i + tab_x_off, tam*j + tab_y_off, tam, tam));
-
     for i in state: 
         for j in range(0, len(state[i])):
             win.blit(images[i], (state[i][j][0]*tam + tab_x_off, state[i][j][1]*tam + tab_y_off)) 
-
+    if not dragdrop:
+        for i in mov:
+            pygame.draw.circle(win, (200, 0, 0), (tam*i[0] + tam//2 + tab_x_off, tam*i[1] + tam // 2 + tab_y_off), tam//4);
     pygame.display.update()
 
 pygame.quit()
